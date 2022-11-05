0353c127249526f0f07375b290f43abc3824429f
SsomGrap <ssomgrap@gmail.com>
2022-11-05 15:00:59 +0100

    create repo

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+<?php include 'templates/head.html'?>
+        <link rel="stylesheet" href="css/index.css">
+        <title>Barbet Sacha</title>
+    </head>
+
+    <body>
+<?php include 'templates/header.html'?>
+
+        <main>
+            <section id="sectionMain">
+
+            </section>
+
+            <section id="sectionProfesionnal">
+
+            </section>
+
+            <section id="sectionPersonnal">
+
+            </section>
+        </main>
+
+<?php include 'templates/footer.html'?>
+    </body>
+</html>
---
+++ template.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+<?php include 'templates/head.html'?>
+        <link rel="stylesheet" href="css/template.css">
+        <title>Template</title>
+    </head>
+
+    <body>
+<?php include 'templates/header.html'?>
+
+        <main>
+
+        </main>
+
+<?php include 'templates/footer.html'?>
+    </body>
+</html>
