85acdb985843cffcde06b4e25f8f33de011be4e9
Sacha <sacha.barbet@gmail.com>
2022-12-02 11:14:12 +0100

    update

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,9 +1,32 @@
-const sections = ["career", "formation", "projects", "contact"];
+const sections = ["main", "career", "formations", "projects", "contact"];
 
-function switchSection(sectionID) {
-    document.getElementsByTagName("section").forEach(element => {
-        element.style.display = "none";
-    });
+function switchSection(section, sectionID) {
+
+    const listID = `link${section[0].toUpperCase() + section.slice(1)}`;
+
+    const listList = document.getElementsByClassName("link");
+    const sectionList = document.getElementsByTagName("section");
+    for(let i = 0; i < sectionList.length; i++) {
+        sectionList[i].style.display = "none";
+        listList[i].style.color = "#ffffff";
+    }
 
     document.getElementById(sectionID).style.display = "flex";
+    document.getElementById(listID).style.color = "#fca311";
+}
+
+async function getSection(section) {
+
+    const sectionID = `section${section[0].toUpperCase() + section.slice(1)}`;
+
+    if(document.getElementById(sectionID) === null && section !== "main") {
+
+        const fetchTable = await fetch(`./templates/${section}.html`);
+        const fetchText = await fetchTable.text();
+    
+        const contentBox = document.getElementById("contentBox").innerHTML;
+        document.getElementById("contentBox").innerHTML = contentBox + fetchText;
+    }
+
+    switchSection(section, sectionID);
 }
