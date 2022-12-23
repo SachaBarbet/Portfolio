63a1f21457c168498611b92e8c0e545931008109
Sacha <sacha.barbet@gmail.com>
2022-12-23 19:39:36 +0100

    update

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,28 +1,27 @@
-const sections = ["main", "formations", "career", "projects", "contact"];
-const sectionsBis = ["enterprise", "task", "project"];
+const sections = ["Main", "Formations", "Career", "Projects", "Contact"];
 var isTemplateBoxChecked = false;
 
-function switchSection(section) {
-
-    const secCap = section[0].toUpperCase() + section.slice(1);
+function switchSection(sectionName) {
 
     const listList = document.getElementsByClassName("link");
+    const sectionList = document.getElementsByTagName("section");
+
     for(let i = 0; i < listList.length; i++) {
         listList[i].style.color = "var(--color-secondary)";
     }
 
-    const sectionList = document.getElementsByTagName("section");
     for(let i = 0; i < sectionList.length; i++) {
         sectionList[i].style.display = "none";
     }
 
-    document.getElementById(`link${secCap}`).style.color = "var(--color-orange)";
-    document.getElementById(`section${secCap}`).style.display = "flex";
+    document.getElementById(`link${sectionName}`).style.color = "var(--color-orange)";
+    document.getElementById(`section${sectionName}`).style.display = "flex";
 }
 
 function changeTheme() {
     const colorRel = document.getElementById("colorRel");
     isTemplateBoxChecked = !isTemplateBoxChecked;
+    
     if (isTemplateBoxChecked) {
         colorRel.setAttribute("href", "css/colors/white.css");
     } else {
