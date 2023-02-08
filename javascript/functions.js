b66344eedb3ca3c88b010a6e7e6be8f8e213bdab
SsomGrap <sacha.barbet@gmail.com>
2023-02-08 19:35:33 +0100

    update

--- javascript/functions.js
+++ javascript/functions.js
@@ -1,32 +1,11 @@
-const sections = ["Main", "Formations", "Career", "Projects"];
 var isTemplateBoxChecked = false;
-
-function switchSection(sectionName) {
-
-    const listList = document.getElementsByClassName("link");
-    const sectionList = document.getElementsByTagName("section");
-
-    for(let i = 0; i < listList.length; i++) {
-        listList[i].classList.remove("linkSelected");
-    }
-
-    for(let i = 0; i < sectionList.length; i++) {
-        sectionList[i].classList.add("none");
-    }
-
-    document.getElementById(`link${sectionName}`).classList.add("linkSelected");
-    document.getElementById(`section${sectionName}`).classList.remove("none");
-}
 
 function changeTheme() {
     const colorRel = document.getElementById("colorRel");
     isTemplateBoxChecked = !isTemplateBoxChecked;
     
-    if (isTemplateBoxChecked) {
-        colorRel.setAttribute("href", "css/colors/white.css");
-    } else {
-        colorRel.setAttribute("href", "css/colors/black.css");
-    }
+    if (isTemplateBoxChecked) return colorRel.setAttribute("href", "css/colors/white.css");
+    return colorRel.setAttribute("href", "css/colors/black.css");
 }
 
 async function generateTimeline(timeline) {
