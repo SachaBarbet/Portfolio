0f96d833f834173d28e3d5aa84a5da1883b968bf
barbetsacha <117525182+SachaBarbet@users.noreply.github.com>
2023-10-07 18:57:31 +0200

    update

--- javascript/themes.js
+++ javascript/themes.js
@@ -1,21 +1,27 @@
 // Switch between dark and light mode.
-
 function switchTheme() {
-    const theme = document.getElementById('css-theme').getAttribute('href');
-    if (theme == 'css/themes/dark.css') {
-        document.getElementById('css-theme').setAttribute('href', 'css/themes/light.css');
-    } else {
-        document.getElementById('css-theme').setAttribute('href', 'css/themes/dark.css');
-    }
+    let element = document.getElementById('themeSwitchInput');
+
+    let themeLink = element.getAttribute('thememode') == "light" ? 'css/themes/light.css' : 'css/themes/dark.css';
+    let iconLink = element.getAttribute('thememode') == "light" ? 'assets/images/light_mode.svg' : 'assets/images/dark_mode.svg';
+    element.getAttribute('thememode') == "light" ? element.style.filter = "" : element.style.filter = "invert(72%) sepia(100%) saturate(344%) hue-rotate(14deg) brightness(111%) contrast(87%)" ;
+    element.setAttribute('src', iconLink);
+    document.getElementById('css-theme').setAttribute('href', themeLink);
 }
 
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.getElementById('css-theme').setAttribute('href', 'css/themes/dark.css');
-} else {
-    document.getElementById('css-theme').setAttribute('href', 'css/themes/light.css');
+function clickThemeIcon() {
+    let element = document.getElementById('themeSwitchInput');
+
+    let theme = element.getAttribute('thememode') == "light" ? "dark" : "light";
+    element.setAttribute("thememode", theme);
+    switchTheme();
 }
 
-window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-    const newColorScheme = e.matches ? "dark" : "light";
-    document.getElementById('css-theme').setAttribute('href', `css/themes/${newColorScheme}.css`);
+let theme = (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) ? "dark" : "light";
+document.getElementById('themeSwitchInput').setAttribute('thememode', theme);
+switchTheme();
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', element => {
+    document.getElementById('themeSwitchInput').setAttribute('thememode', element.matches ? "dark" : "light");
+    switchTheme();
 });
